46d172915cbae2e4138d9d6068cb6f9db938a736
TTuCToJlEToB <aleksandrpistoletov708@gmail.com>
2025-08-30 23:39:12 +0300

    WIP: NewsCarousel. Added mocked data

--- Schoolars-Web/src/App.tsx
+++ Schoolars-Web/src/App.tsx
@@ -1,10 +1,11 @@
 import { CarouselDemo } from './components/ui/democarousel.js'
+import { News } from './mockedData/DataBase.js'
 function App() {
 	return (
 		<div
 			style={{ display: 'flex', justifyContent: 'center', marginTop: '5vh' }}
 		>
-			<CarouselDemo />
+			<CarouselDemo News={News} />
 		</div>
 	)
 }
--- Schoolars-Web/src/components/ui/democarousel.tsx
+++ Schoolars-Web/src/components/ui/democarousel.tsx
@@ -8,23 +8,38 @@ import {
 	CarouselPrevious,
 } from '@/components/ui/carousel'
 
-export function CarouselDemo() {
+//@ts-ignore
+
+export function CarouselDemo({ News }) {
 	return (
 		<div className='relative w-[75vw] h-[70vh]'>
 			<Carousel
 				opts={{
 					align: 'start',
+					loop: true,
 				}}
 				className='w-full h-full'
 				style={{ alignContent: 'center' }}
 			>
 				<CarouselContent className='h-full'>
-					{Array.from({ length: 5 }).map((_, index) => (
+					{[...News].map((part, index) => (
 						<CarouselItem key={index} className='basis-full h-full'>
 							<div className='p-1 h-full'>
 								<Card className='h-full min-w-[150px] min-h-[300px] max-h-[1000px]'>
 									<CardContent className='flex items-center justify-center p-6 h-full min-h-[300px] max-h-[1000px]'>
-										<span className='text-3xl font-semibold'>{index + 1}</span>
+										<div
+											style={{
+												display: 'flex',
+												flexDirection: 'column',
+											}}
+										>
+											<h2 className='text-[clamp(10px,2rem,4.5rem)] scroll-m-20 border-b pb-3 text-3xl font-semibold tracking-tight first:mt-0'>
+												{part.title}
+											</h2>
+											<p className='text-[clamp(8px,1rem,3rem)] leading-7 [&:not(:first-child)]:mt-8'>
+												{part.body}
+											</p>
+										</div>
 									</CardContent>
 								</Card>
 							</div>
